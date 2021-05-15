ebe8dddc14fdeab57405f1a4a51a83ae0c8d85c3
pngwn <hello@pngwn.io>
2021-05-15 20:20:54 +0100

    fix mobile view

--- src/store/places.js
+++ src/store/places.js
@@ -1,28 +1,28 @@
-import { readable } from 'svelte/store';
+import { readable } from "svelte/store";
 
-export const places = readable(set => set([
+export const places = readable([
   {
-    name: 'Honolulu',
+    name: "Honolulu",
     stars: 4,
     rating: 8.9,
-    img: '/honolulu.jpg',
+    img: "/honolulu.jpg",
     description:
-      'Ocean breezes rustle palm trees along the harborfront, while in the cool, mist-shrouded Koʻolau Range, forested hiking trails offer postcard city views. At sunset, cool off with an amble around Magic Island or splash in the ocean at Ala Moana Beach.'
+      "Ocean breezes rustle palm trees along the harborfront, while in the cool, mist-shrouded Koʻolau Range, forested hiking trails offer postcard city views. At sunset, cool off with an amble around Magic Island or splash in the ocean at Ala Moana Beach.",
   },
   {
-    name: 'Santorini',
+    name: "Santorini",
     stars: 4,
     rating: 7.8,
-    img: '/santorini.jpg',
+    img: "/santorini.jpg",
     description:
-      'With multicoloured cliffs soaring above a sea-drowned caldera, Santorini looks like a giant slab of layered cake. The main island of Thira will take your breath away with its snow-drift of white Cycladic houses lining the cliff tops and, in places, spilling like icy cornices down the terraced rock.'
+      "With multicoloured cliffs soaring above a sea-drowned caldera, Santorini looks like a giant slab of layered cake. The main island of Thira will take your breath away with its snow-drift of white Cycladic houses lining the cliff tops and, in places, spilling like icy cornices down the terraced rock.",
   },
   {
-    name: 'Cusco',
+    name: "Cusco",
     stars: 3,
     rating: 7.4,
-    img: '/peru.jpg',
+    img: "/peru.jpg",
     description:
-      'Wandered the cobblestone streets and quaint lanes of the town, which has been designated a UNESCO World Heritage site. A walking tour revealed historic architecture, colonial landmarks and alluring shops and restaurants.'
-  }
-]));
+      "Wandered the cobblestone streets and quaint lanes of the town, which has been designated a UNESCO World Heritage site. A walking tour revealed historic architecture, colonial landmarks and alluring shops and restaurants.",
+  },
+]);
