ce5396b917763b53440ebb86d58f0f79bb71246c
pngwn <hello@pngwn.io>
2021-05-16 00:23:26 +0100

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
+
+// console.log(adapter());
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter: null,
-    trailingSlash: "always",
+    adapter: adapter(),
+    files: {
+      assets: "assets",
+    },
   },
   preprocess: preprocess(),
 };
